ea703fc783cdbbcc9c0415c4717cfd6478a50f17
daazarov <daazarov@telecom.by>
2024-01-29 17:18:39 +0300

    init commit

---
+++ src/EVEOnline.Esi.Communication.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace EVEOnline.Esi.Communication.IntegrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ src/EVEOnline.Esi.Communication.UnitTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+namespace EVEOnline.Esi.Communication.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Warn("Test warning");
+        }
+    }
+}
