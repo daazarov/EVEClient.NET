c29c4ad2819b2a73843861e4a8d3480e59f61a3e
daazarov <daazarov@telecom.by>
2024-02-02 12:34:58 +0300

    fix build

--- src/EVEOnline.Esi.Communication/DataContract/Enumerations/ContractAvailability.cs
+++ src/EVEOnline.Esi.Communication/DataContract/Enumerations/ContractAvailability.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Converters;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using System.Runtime.Serialization;
 
 namespace EVEOnline.Esi.Communication.DataContract
