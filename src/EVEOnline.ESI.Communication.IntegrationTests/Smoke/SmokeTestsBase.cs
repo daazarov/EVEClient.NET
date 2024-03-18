629b519d6308ead9754ee792ebd9c41987b7add9
daazarov <daazarov@telecom.by>
2024-03-18 13:08:01 +0300

    refactor smoke tests

---
+++ src/EVEOnline.ESI.Communication.IntegrationTests/Smoke/SmokeTestsBase.cs
@@ -0,0 +1,44 @@
+﻿using EVEOnline.ESI.Communication.Utilities.Stores;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace EVEOnline.ESI.Communication.IntegrationTests.Smoke
+{
+    [TestFixture]
+    [Category("SmokeTest")]
+    public class SmokeTestsBase
+    {
+        protected ServiceProvider _serviceProvider;
+        protected ServiceCollection _serviceCollection;
+        protected IEsiLogicAccessor _logicAccessor;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            _serviceCollection = new ServiceCollection();
+
+            _serviceCollection.AddEVEOnlineEsiClient(config =>
+            {
+                config.Server = "tranquility";
+                config.UserAgent = "github.com/daazarov/EVEOnline.ESI.Communication smoke tests";
+                config.BaseUrl = "https://esi.evetech.net";
+                config.EnableETag = true;
+            });
+
+            _serviceProvider = _serviceCollection.BuildServiceProvider();
+            _logicAccessor = _serviceProvider.GetService<IEsiLogicAccessor>()!;
+        }
+
+        [SetUp]
+        public void PerTestsSetup()
+        {
+            (_serviceProvider.GetService<IETagStorage>() as DefaultInMemoryETagThreadSaveStore)!.Clear();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            _serviceProvider.Dispose();
+        }
+    }
+}
--- src/EVEOnline.ESI.Communication.IntegrationTests/Smoke/SomePublicEndpointsTests.cs
+++ src/EVEOnline.ESI.Communication.IntegrationTests/Smoke/SomePublicEndpointsTests.cs
@@ -1,54 +1,12 @@
-﻿using EVEOnline.ESI.Communication.Utilities.Stores;
-using Microsoft.Extensions.DependencyInjection;
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using NUnit.Framework;
 using System.Net;
-using System.Security.Authentication.ExtendedProtection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EVEOnline.ESI.Communication.IntegrationTests.Smoke
 {
     [TestFixture]
-    internal class SomePublicEndpointsTests
+    public class SomePublicEndpointsTests : SmokeTestsBase
     {
-        private ServiceProvider _serviceProvider;
-        private ServiceCollection _serviceCollection;
-        private IEsiLogicAccessor _logicAccessor;
-
         private const int CharacterId = 2119944183;
-
-        [OneTimeSetUp]
-        public void OneTimeSetup()
-        {
-            _serviceCollection = new ServiceCollection();
-
-            _serviceCollection.AddEVEOnlineEsiClient(config =>
-            {
-                config.Server = "tranquility";
-                config.UserAgent = "github.com/daazarov/EVEOnline.ESI.Communication smoke tests";
-                config.BaseUrl = "https://esi.evetech.net";
-                config.EnableETag = true;
-            });
-
-            _serviceProvider = _serviceCollection.BuildServiceProvider();
-
-            _logicAccessor = _serviceProvider.GetService<IEsiLogicAccessor>()!;
-        }
-
-        [SetUp]
-        public void PerTestsSetup()
-        {
-            (_serviceProvider.GetService<IETagStorage>() as DefaultInMemoryETagThreadSaveStore)!.Clear();
-        }
-
-        [OneTimeTearDown]
-        public void OneTimeTearDown()
-        {
-            _serviceProvider.Dispose();
-        }
 
         [Test]
         public async Task GetCharacterInfo()
