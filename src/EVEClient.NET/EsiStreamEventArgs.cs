e5d3eb6a64a16b4da07878dcdbe58ac20809ca72
daazarov <daazarov@telecom.by>
2024-09-18 17:24:48 +0300

    issue#46 - implement EsiStreamResponse

---
+++ src/EVEClient.NET/EsiStreamEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EVEClient.NET
+{
+    public class EsiStreamEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets the <see cref="EsiResponse"/>.
+        /// </summary>
+        public EsiResponse Response { get; }
+
+        /// <summary>
+        /// Gets the requested page number.
+        /// </summary>
+        public int RequestedPage { get; }
+
+        internal EsiStreamEventArgs(EsiResponse response, int page)
+        {
+            ArgumentNullException.ThrowIfNull(response);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(page);
+
+            Response = response;
+            RequestedPage = page;
+        }
+    }
+}
---
+++ src/EVEClient.NET/IEsiStateEnumerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace EVEClient.NET
+{
+    internal interface IEsiStateEnumerator<T> : IAsyncEnumerator<T>
+    {
+        State CurrentState { get; set; }
+
+        public int NextPage { get; set; }
+
+        public int LastPage { get; set; }
+    }
+
+    internal enum State : byte
+    {
+        Initial,
+        Running,
+        Completed,
+        Disposed
+    }
+}
